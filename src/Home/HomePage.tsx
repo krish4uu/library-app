8b06ef6b975f526976e969e5f6e4977bc1426415
krish4uu <prajapatikashyap97@gmail.com>
2024-04-01 23:23:23 -0400

    Created HomePage component

---
+++ src/Home/HomePage.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { List } from "../ListItems/List";
+import { Header } from "../Header/Header";
+import { SearchBar } from "../SearchBar/SearchBar";
+import { HomePageSvg } from "../Utilities/HomePageSvg";
+
+const HomePage = () => {
+  return (
+    <div className="container flex place-content-center max-w-full ">
+      <div>
+        <div>
+          <Header />
+          <SearchBar />
+        </div>
+        <div className="flex gap-6 mt-2 pt-2 relative">
+          <HomePageSvg />
+          <List />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HomePage;
