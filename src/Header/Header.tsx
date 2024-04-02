84965eda3898ed549d2aa1fbfc94804e1d42ed85
krish4uu <prajapatikashyap97@gmail.com>
2024-04-01 23:21:16 -0400

    Created Header component

---
+++ src/Header/Header.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { IoMdAddCircle } from "react-icons/io";
+
+export const Header = () => {
+  return (
+    <div className="flex justify-between mt-14 pt-2">
+      <div className="flex gap-6">
+        <span className="p-4 invisible"></span>
+        <header className="flex text-4xl font-semibold">
+          <h1>Notes</h1>
+        </header>
+      </div>
+
+      <button>
+        <IoMdAddCircle size={`3rem`} fill="lightgreen" />
+      </button>
+    </div>
+  );
+};
