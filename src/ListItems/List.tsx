90ad6258365984b3441a4a698b8c41c2856bce46
krish4uu <prajapatikashyap97@gmail.com>
2024-04-02 15:50:54 -0400

    Fix: List item hover state bug

--- src/ListItems/List.tsx
+++ src/ListItems/List.tsx
@@ -6,75 +6,91 @@ import { listArray } from "../ListsArray";
 import { ListItem } from "../ListsArray";
 
 export const List: React.FC = () => {
-  const [mouseOver, setMouseOver] = useState<boolean>(false);
+  const [hoverStates, setHoverStates] = useState<{ [key: number]: boolean }>(
+    {}
+  );
+
+  const handleMouseEnter = (itemId: number) => {
+    setHoverStates((prevState) => ({
+      ...prevState,
+      [itemId]: true,
+    }));
+  };
+
+  const handleMouseLeave = (itemId: number) => {
+    setHoverStates((prevState) => ({
+      ...prevState,
+      [itemId]: false,
+    }));
+  };
 
   return (
     <>
       <ul className="flex flex-col w-full">
         {listArray.map((item: ListItem) => (
-          <>
-            <li key={item.id}>
-              <div
-                className="flex gap-6 mt-2 pt-2 pb-1"
-                onMouseEnter={(e) => setMouseOver(true)}
-                onMouseLeave={() => setMouseOver((prev) => !prev)}
-              >
+          <li key={item.id}>
+            <div
+              className="flex gap-6 mt-2 pt-2 pb-1"
+              onMouseEnter={() => handleMouseEnter(item.id)}
+              onMouseLeave={() => handleMouseLeave(item.id)}
+            >
+              <button type="submit">
+                <MdOutlinePushPin
+                  size={`2rem`}
+                  fill="skyblue"
+                  className={`rotate-45 ${
+                    hoverStates[item.id] ? "visible" : "invisible"
+                  }`}
+                  onClick={(e) => e.preventDefault()}
+                />
+              </button>
+              <div className="flex justify-between w-full">
+                <header className="text-2xl font-semibold max-w-screen-sm flex-wrap text-gray-500">
+                  <h1>{item.title}</h1>
+                </header>
                 <button type="submit">
-                  <MdOutlinePushPin
+                  <MdDeleteOutline
                     size={`2rem`}
-                    fill="skyblue"
-                    className={`rotate-45 ${
-                      mouseOver ? "visible" : "invisible"
+                    fill="red"
+                    className={`${
+                      hoverStates[item.id] ? "visible" : "invisible"
                     }`}
                     onClick={(e) => e.preventDefault()}
                   />
                 </button>
-                <div className="flex justify-between w-full">
-                  <header className="text-2xl font-semibold max-w-screen-sm flex-wrap text-gray-500">
-                    <h1>{item.title}</h1>
-                  </header>
-                  <button type="submit">
-                    <MdDeleteOutline
-                      size={`2rem`}
-                      fill="red"
-                      className={`${mouseOver ? "visible" : "invisible"}`}
-                      onClick={(e) => e.preventDefault()}
-                    />
-                  </button>
+              </div>
+            </div>
+            <div className="flex gap-2 justify-between items-end">
+              <div className="flex gap-6 ">
+                <span className="p-4 invisible"></span>
+                <div className="flex gap-2 items-end">
+                  <MdOutlinePersonAddAlt size={`2rem`} fill="gray" />
+                  {item.collaborators ? (
+                    <>
+                      {item.collaboratorsName.length > 1 ? (
+                        <>
+                          <p className="text-gray-500">
+                            {item.collaboratorsName
+                              .toString()
+                              .replaceAll(",", ` and `)}
+                          </p>
+                        </>
+                      ) : (
+                        <>
+                          <p className="text-gray-500">
+                            {item.collaboratorsName.join(",")}
+                          </p>
+                        </>
+                      )}
+                    </>
+                  ) : (
+                    <p className="text-gray-500">"Add Collaborators"</p>
+                  )}
                 </div>
               </div>
-              <div className="flex gap-2 justify-between items-end">
-                <div className="flex gap-6 ">
-                  <span className="p-4 invisible"></span>
-                  <div className="flex gap-2 items-end">
-                    <MdOutlinePersonAddAlt size={`2rem`} fill="gray" />
-                    {item.collaborators ? (
-                      <>
-                        {item.collaboratorsName.length > 1 ? (
-                          <>
-                            <p className="text-gray-500">
-                              {item.collaboratorsName
-                                .toString()
-                                .replaceAll(",", ` and `)}
-                            </p>
-                          </>
-                        ) : (
-                          <>
-                            <p className="text-gray-500">
-                              {item.collaboratorsName.join(",")}
-                            </p>
-                          </>
-                        )}
-                      </>
-                    ) : (
-                      <p className="text-gray-500">"Add Collaborators"</p>
-                    )}
-                  </div>
-                </div>
-                <p className="text-sm text-gray-400 italic">{item.time}</p>
-              </div>
-            </li>
-          </>
+              <p className="text-sm text-gray-400 italic">{item.time}</p>
+            </div>
+          </li>
         ))}
       </ul>
     </>
