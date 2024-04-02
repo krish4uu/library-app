f4152f5df68829dc0f28937789194066f6599328
krish4uu <prajapatikashyap97@gmail.com>
2024-04-01 23:21:34 -0400

    Created SearchBar component

---
+++ src/SearchBar/SearchBar.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { IoIosSearch } from "react-icons/io";
+
+export const SearchBar = () => {
+  return (
+    <div className="flex">
+      <form className="flex gap-6 mt-4 w-full">
+        <button className="rotate-45" type="submit">
+          <IoIosSearch
+            size={`2rem`}
+            fill="lightgray"
+            className="rotate-45"
+            onClick={(e) => e.preventDefault()}
+          />
+        </button>
+        <input type="text" size={70} placeholder="Search through your notes" />
+      </form>
+    </div>
+  );
+};
